537da33ac0fbd3692e8934d3fbb622a442185ac8
Lisa M <martelizaveta_r@mail.ru>
2024-12-09 17:00:47 +0800

    поменяли q0

--- pytorch_code/model.py
+++ pytorch_code/model.py
@@ -210,7 +210,10 @@ class LastAttenion(nn.Module):
 
         # Для q0
         q0 = self.linear_zero(ht1)  # (batch_size, hidden_size)
-        q0 = q0.view(batch_size, 1, self.hidden_size // self.heads)  # (batch_size, 1, hidden_size // heads)
+        q0 = q0.view(batch_size, 1, self.hidden_size)  # (batch_size, 1, hidden_size)
+
+        # Теперь мы расширяем q0 для seq_len
+        q0 = q0.expand(-1, seq_len, -1)  # (batch_size, seq_len, hidden_size)
 
         # Для q1
         q1 = self.linear_one(hidden)  # (batch_size, seq_len, hidden_size)
@@ -225,15 +228,12 @@ class LastAttenion(nn.Module):
                      self.hidden_size // self.heads)  # (batch_size, seq_len, heads, hidden_size // heads)
         q2 = q2.permute(0, 2, 1, 3).contiguous()  # (batch_size, heads, seq_len, hidden_size // heads)
 
-        # Используем q0 и q1 для матричного умножения
-        q0 = q0.expand(-1, seq_len, -1)  # (batch_size, seq_len, hidden_size // heads)
+        # 1. Вычисляем alpha
+        alpha = torch.sigmoid(torch.matmul(q0, q1.permute(0, 3, 2, 1)))  # (batch_size, seq_len, seq_len)
 
         print(f"--- Debugging --- q0.shape: {q0.shape}")
         print(f"--- Debugging --- q1.shape: {q1.shape}")
         print(f"--- Debugging --- q2.shape: {q2.shape}")
-
-        #1. Вычисляем alpha
-        alpha = torch.sigmoid(torch.matmul(q0, q1.permute(0, 3, 2, 1)))  # (batch_size, seq_len, seq_len)
 
         # 2. Перераспределение alpha для softmax
         alpha = alpha.view(-1, q0.size(1) * self.heads, hidden.size(1)).permute(0, 2, 1)
